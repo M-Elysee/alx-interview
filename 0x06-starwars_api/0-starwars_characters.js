b912d27643db82a54102c97f4b34dc657653b91b
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2024-02-15 17:19:20 +0000

    0x06. Star Wars API| debug5

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,48 +2,37 @@
 // a script that fetch and prints all characters of a Star Wars movie
 
 const request = require('request');
-let id;
 
-try {
-  id = process.argv[2];
-  id = parseInt(id);
-  if (!Number.isInteger(id)) {
-    throw new Error();
-  }
-  if (!id) {
-    throw new Error();
-  }
-} catch (error) {
-  process.exit(1);
-}
+if (process.argv.length > 2) {
+  const id = process.argv[2];
+  request({
+    method: 'GET',
+    uri: `https://swapi-api.alx-tools.com/api/films/${id}`
+  },
+  function (error, response, body) {
+    if (error) {
+      console.error(error);
+    } else if (response && response.statusCode === 200) {
+      const characters = JSON.parse(body).characters;
+      // console.log(characters);
+      fetchCharacters(characters);
+    }
+  });
 
-request({
-  method: 'GET',
-  uri: `https://swapi-api.alx-tools.com/api/films/${id}`
-},
-function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else if (response && response.statusCode === 200) {
-    const characters = JSON.parse(body).characters;
-    // console.log(characters);
-    fetchCharacters(characters);
-  }
-});
-
-function fetchCharacters (characters) {
-  for (const character of characters) {
-    request({
-      method: 'GET',
-      uri: `${character}`
-    },
-    function (error, response, body) {
-      if (error) {
-        console.error(error);
-      } else if (response && response.statusCode === 200) {
-        const name = JSON.parse(body).name;
-        console.log(name);
-      }
-    });
+  function fetchCharacters (characters) {
+    for (const character of characters) {
+      request({
+        method: 'GET',
+        uri: `${character}`
+      },
+      function (error, response, body) {
+        if (error) {
+          console.error(error);
+        } else if (response && response.statusCode === 200) {
+          const name = JSON.parse(body).name;
+          console.log(name);
+        }
+      });
+    }
   }
 }
