3c84ddc0930a27dd378cdb5d33f7066ed85a3e2c
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2024-02-15 17:41:28 +0000

    0x06. Star Wars API| debug6

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -14,25 +14,31 @@ if (process.argv.length > 2) {
       console.error(error);
     } else if (response && response.statusCode === 200) {
       const characters = JSON.parse(body).characters;
-      // console.log(characters);
-      fetchCharacters(characters);
+      fetchCharacters(characters)
+        .catch(error => console.error(error));
     }
   });
 
-  function fetchCharacters (characters) {
+  async function fetchCharacters (characters) {
     for (const character of characters) {
+      const name = await getCharacterName(character);
+      console.log(name);
+    }
+  }
+
+  function getCharacterName (character) {
+    return new Promise((resolve, reject) => {
       request({
         method: 'GET',
-        uri: `${character}`
-      },
-      function (error, response, body) {
+        uri: character
+      }, (error, response, body) => {
         if (error) {
-          console.error(error);
+          reject(error);
         } else if (response && response.statusCode === 200) {
           const name = JSON.parse(body).name;
-          console.log(name);
+          resolve(name);
         }
       });
-    }
+    });
   }
 }
